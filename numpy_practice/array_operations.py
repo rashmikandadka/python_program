a93b9e637f4494302f2ca81148df75696617e1f2
rashmikandadka <165014936+rashmikandadka@users.noreply.github.com>
2026-03-06 16:43:41 +0530

    Add array operations using NumPy

---
+++ numpy_practice/array_operations.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Creating arrays
+a = np.array([1, 2, 3, 4])
+b = np.array([5, 6, 7, 8])
+
+print("Array A:", a)
+print("Array B:", b)
+
+print("Addition:", a + b)
+print("Multiplication:", a * b)
