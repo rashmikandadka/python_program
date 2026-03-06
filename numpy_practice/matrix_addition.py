f320f3f3ec77cf9fdd7c50484c448717daf92798
rashmikandadka <165014936+rashmikandadka@users.noreply.github.com>
2026-03-06 16:48:51 +0530

    Create matrix_addition.py

---
+++ numpy_practice/matrix_addition.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Create matrices
+A = np.array([[1,2],[3,4]])
+B = np.array([[5,6],[7,8]])
+
+# Add matrices
+C = A + B
+
+print("Matrix A:")
+print(A)
+
+print("Matrix B:")
+print(B)
+
+print("Matrix Addition Result:")
+print(C)
