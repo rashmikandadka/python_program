533ce91fe52114aff6cf09e80749977505856942
rashmikandadka <165014936+rashmikandadka@users.noreply.github.com>
2026-03-06 16:49:55 +0530

    Add statistics calculations using NumPy

---
+++ numpy_practice/statistics.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+data = np.array([10, 20, 30, 40, 50])
+
+print("Data:", data)
+print("Mean:", np.mean(data))
+print("Median:", np.median(data))
+print("Standard Deviation:", np.std(data))
